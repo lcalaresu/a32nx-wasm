4b0b53ec58e3d6d09669103e4b799ffd0b86ccfc
ThePilot916 <19603746+ThePilot916@users.noreply.github.com>
2020-09-28 12:12:33 +0530

    PID controller now accepts max +ve and -ve change the system is capable of outputting.

--- common_sys.h
+++ common_sys.h
@@ -464,6 +464,8 @@ private:
     double Ei;          //predicted error
     double Ed;          //rate of error change
     double lastEp;      //last proportional error
+    double _max_pos_change;  //max +ve change the system is capable of outputting 
+    double _max_neg_change;  //max -ve change the system is capable of outputting
     double lastUpdateTime;
 
     bool crossZero() {
@@ -476,7 +478,7 @@ private:
         const double deltaT = currentTime - lastUpdateTime;
         Ep = currentError;
         if (crossZero()) {
-            Ei = 0;                 //predicted(integral) error is reset to 0 when it crosses zero.
+            Ei = 0;                 //predicted(integral) error is reset to 0 when Ep(currentError) crosses zero.
         }
         Ei += (Ep * deltaT);
         if (deltaT != 0) {
@@ -486,14 +488,17 @@ private:
         lastUpdateTime = currentTime;
     }
 public:
-    void init(const double Kp, const double Ki, const double Kd, const double currentAbsTime) {
+    void init(const double Kp, const double Ki, const double Kd, const double currentAbsTime, const double max_pos_change, const double max_neg_change) {
         _Kp = Kp;
         _Ki = Ki;
         _Kd = Kd;
         Ep = 0;
         Ei = 0;
         Ed = 0;
+        _max_pos_change = max_pos_change;
+        _max_neg_change = max_neg_change;
         lastEp = 0;
+        
         lastUpdateTime = currentAbsTime;
     }
     /*
@@ -501,7 +506,14 @@ public:
     */
     double control(const double currentError, const double currentAbsTime){
         updateError(currentError, currentAbsTime);
-        return (_Kp * Ep + _Ki * Ei + _Kd * Ed);
+        double adjust_var =  (_Kp * Ep + _Ki * Ei + _Kd * Ed);
+        if(adjust_var > _max_pos_change){
+            adjust_var = _max_pos_change;
+        }
+        if(adjust_var < _max_neg_change){
+            adjust_var = _max_neg_change;
+        }
+        return adjust_var;
     }
 };
 
