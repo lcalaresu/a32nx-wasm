c81c85edc4af99b9713cd31c47e93316d70e1629
ThePilot916 <19603746+ThePilot916@users.noreply.github.com>
2020-10-05 19:27:57 +0530

    added comments, improved getbit

--- utils/bit_oper.h
+++ utils/bit_oper.h
@@ -1,10 +1,18 @@
 #pragma once
 
+/*
+* ============== *
+* Bit Operations *
+* ============== *
+*/
 class bitOper {
 private:
 public:
     bool getbit(unsigned long int num, int bit_loc) {
-        return((num & (1 << bit_loc)) << 63 - bit_loc >> 63);
+        /*
+        return(((num & (1 << bit_loc)) << 63 - bit_loc) >> 63);
+        */
+        return ((num & (1 << bit_loc) == 0)? 0 : 1);
     }
     unsigned int setbit(unsigned long int num, int bit_loc) {
         return (num | (1 << bit_loc));
