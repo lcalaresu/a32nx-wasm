7f2b8dbc5f13a19ded41e5c9456db0e33ba5c38b
ThePilot916 <19603746+ThePilot916@users.noreply.github.com>
2020-10-13 10:14:56 +0530

    added some debug print statements

---
+++ wasm_sys.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//currently not in use will be used at a later stage for file structure improvements and mainly with in-process wasmmodule integration
+#ifndef __INTELLISENSE__
+#	define MODULE_EXPORT __attribute__( ( visibility( "default" ) ) )
+#	define MODULE_WASM_MODNAME(mod) __attribute__((import_module(mod)))
+#else
+#	define MODULE_EXPORT
+#	define MODULE_WASM_MODNAME(mod)
+#	define __attribute__(x)
+#	define __restrict__
+#endif
