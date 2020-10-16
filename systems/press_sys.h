199418aaefca1a5f8b78d1e40f6a188b24be17ae
ThePilot916 <shreyasmiraj8@gmail.com>
2020-10-17 01:25:33 +0530

    added press basic framework

--- systems/press_sys.h
+++ systems/press_sys.h
@@ -4,11 +4,49 @@
 
 class PressSys {
 private:
-public:
-    void init() {
+    bool takeoff;
+    bool landed;
+    double landing_timer;
+
+    void incCabinAlt(double inc) {
+    
+    }
+    void decCabinAlt(double dec) {
 
     }
-    void update() {
+    void setCabinAlt(double alt) {
 
+    }
+    void incCabinVS(double alt) {
+
+    }
+    void decCabinVS(double alt){
+    
+    }
+    void setLDGELEV(double alt) {
+
+    }
+    void autoSetCabinAlt(double alt) {
+
+    }
+    void autoSetCabinVS(double alt) {
+
+    }
+
+public:
+    void init() {
+        for (int i = OUTFLOW_VALVE; i <= CPC_SYS2; i++) {
+            lSimVarsValue[i] = 0;
+        }
+        takeoff = false;
+        landed = false;
+        lSimVarsValue[OUTFLOW_VALVE] = 100;
+        lSimVarsValue[CPC_SYS1] = 1;
+        lSimVarsValue[CPC_SYS2] = 1;
+        lSimVarsValue[CABIN_ALTITUDE] = aSimVarsValue[ALTITUDE];
+        lSimVarsValue[CABIN_ALTITUDE_GOAL] = aSimVarsValue[ALTITUDE];
+    }
+    void update() {
+        
     }
 };
